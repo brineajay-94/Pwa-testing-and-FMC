181c7d2fb504bf64027e076d621f9bd1e9766e0a
brineajay-94 <ajaykafle254@gmail.com>
2026-01-30 13:56:50 +0545

    Initialize Firebase messaging in service worker
    Set up Firebase messaging service worker with notification handling.

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,1 +1,23 @@
+importScripts("https://www.gstatic.com/firebasejs/12.8.0/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/12.8.0/firebase-messaging.js");
 
+// Initialize Firebase inside SW
+firebase.initializeApp({
+  apiKey: "AIzaSyCPSfmHJwV_1MNNeBF3obnpWaK0jDea6lE",
+  authDomain: "dev-a-3cf61.firebaseapp.com",
+  projectId: "dev-a-3cf61",
+  storageBucket: "dev-a-3cf61.firebasestorage.app",
+  messagingSenderId: "523395724429",
+  appId: "1:523395724429:web:83bfb8a4077442ef611ec4",
+  measurementId: "G-T2FL9C69S7"
+});
+
+const messaging = firebase.messaging();
+
+// Show notification when app is in background or closed
+messaging.onBackgroundMessage((payload) => {
+  self.registration.showNotification(payload.notification.title, {
+    body: payload.notification.body,
+    icon: payload.notification.icon || "/icons/icon-192x192.png"
+  });
+});
